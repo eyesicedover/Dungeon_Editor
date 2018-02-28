737636c73227c6cf2646e60f08263ed4d3a203ea
Andy Grossberg, Michael Albers and Stephanie Faber <stephanie@>
2018-02-28 11:48:56 -0800

    add detail and update views for Rooms

--- Dungeon/Controllers/RoomController.cs
+++ Dungeon/Controllers/RoomController.cs
@@ -11,7 +11,33 @@ namespace Dungeon.Controllers
       [HttpGet("/rooms")]
       public ActionResult Index()
       {
-        return View("RoomIndex");
+        List<Room> allRooms = Room.GetAll();
+        return View("RoomIndex", allRooms);
+      }
+
+      [HttpPost("/rooms")]
+      public ActionResult Create()
+      {
+        string name = Request.Form["newRoomName"];
+        Room newRoom = new Room(name);
+        newRoom.Save();
+        List<Room> allRooms = Room.GetAll();
+        return View("RoomIndex", allRooms);
+      }
+
+      [HttpPost("/rooms/details/{id}")]
+      public ActionResult Details()
+      {
+        Room thisRoom = Room.Find(Int32.Parse(Request.Form["id"]));
+        return View("RoomDetails", thisRoom);
+      }
+
+      [HttpPost("/rooms/update/{id}")]
+      public ActionResult Details(int id)
+      {
+        Room thisRoom = Room.Find(id);
+        thisRoom.Update(Request.Form["updatedRoomName"]);
+        return View("RoomDetails", thisRoom);
       }
 
   }
